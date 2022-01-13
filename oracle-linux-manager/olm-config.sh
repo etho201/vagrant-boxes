36d64fd464f5a88816c8e22e4a6bfa5bd124306c
Erik Thomsen <ethomsen86@gmail.com>
2022-01-13 15:58:26 -0500

    Auto-subscribe systems to Ksplice channels

--- oracle-linux-manager/olm-config.sh
+++ oracle-linux-manager/olm-config.sh
@@ -19,10 +19,12 @@ log "Adding channels and repositories for Ksplice"
 spacecmd -- softwarechannel_create -l oraclelinux7-x86_64-ksplice -n "Ksplice for Oracle Linux 7 (x86_64)" -p oraclelinux7-x86_64 -a x86_64
 spacecmd -- repo_create -n "External - Ksplice for Oracle Linux 7 (x86_64)" -u "uln:///ol7_x86_64_ksplice" -t uln
 spacecmd -- softwarechannel_addrepo oraclelinux7-x86_64-ksplice "External - Ksplice for Oracle Linux 7 (x86_64)"
+spacecmd -- activationkey_addchildchannels 1-oraclelinux7-x86_64 oraclelinux7-x86_64-ksplice
 
 spacecmd -- softwarechannel_create -l oraclelinux8-x86_64-ksplice -n "Ksplice for Oracle Linux 8 (x86_64)" -p oraclelinux8-x86_64 -a x86_64
 spacecmd -- repo_create -n "External - Ksplice for Oracle Linux 8 (x86_64)" -u "uln:///ol8_x86_64_ksplice" -t uln
 spacecmd -- softwarechannel_addrepo oraclelinux8-x86_64-ksplice "External - Ksplice for Oracle Linux 8 (x86_64)"
+spacecmd -- activationkey_addchildchannels 1-oraclelinux8-x86_64 oraclelinux8-x86_64-ksplice
 
 log "Register the Spacewalk server as a client to itself"
 yum install rhn-client-tools rhn-check rhn-setup rhnsd m2crypto yum-rhn-plugin -y
