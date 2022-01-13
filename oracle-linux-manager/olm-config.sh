1f8fd96f3a858dedfc06deab514157a53bec6923
Erik Thomsen <ethomsen86@gmail.com>
2022-01-13 13:33:40 -0500

    Now logging installation steps

--- oracle-linux-manager/olm-config.sh
+++ oracle-linux-manager/olm-config.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+log() {
+    echo "INSTALLER: ${*}" | tee -a /tmp/bootstrap.log
+}
+
+log "Configuring the software channels, repositories, GPG keys, and activation keys"
 spacewalk-common-channels -v -u admin -p Password1 -a x86_64 -k unlimited 'oraclelinux7*'
 spacewalk-common-channels -v -u admin -p Password1 -a x86_64 -k unlimited 'oraclelinux8*'
 
+log "Configuring spacecmd"
 mkdir ~/.spacecmd
 echo '[spacecmd]
 username=admin
 password=Password1' > ~/.spacecmd/config
 chmod 400 ~/.spacecmd/config
 
+log "Adding channels and repositories for Ksplice"
 spacecmd -- softwarechannel_create -l oraclelinux7-x86_64-ksplice -n "Ksplice for Oracle Linux 7 (x86_64)" -p oraclelinux7-x86_64 -a x86_64
 spacecmd -- repo_create -n "External - Ksplice for Oracle Linux 7 (x86_64)" -u "uln:///ol7_x86_64_ksplice" -t uln
 spacecmd -- softwarechannel_addrepo oraclelinux7-x86_64-ksplice "External - Ksplice for Oracle Linux 7 (x86_64)"
@@ -17,6 +24,6 @@ spacecmd -- softwarechannel_create -l oraclelinux8-x86_64-ksplice -n "Ksplice fo
 spacecmd -- repo_create -n "External - Ksplice for Oracle Linux 8 (x86_64)" -u "uln:///ol8_x86_64_ksplice" -t uln
 spacecmd -- softwarechannel_addrepo oraclelinux8-x86_64-ksplice "External - Ksplice for Oracle Linux 8 (x86_64)"
 
-# Register the Spacewalk server as a client to itself
+log "Register the Spacewalk server as a client to itself"
 yum install rhn-client-tools rhn-check rhn-setup rhnsd m2crypto yum-rhn-plugin -y
 rhnreg_ks --sslCACert=/usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT --serverUrl=http://192.168.56.10/XMLRPC --activationkey=1-oraclelinux7-x86_64
--- oracle-linux-manager/provision.sh
+++ oracle-linux-manager/provision.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
-yum update
+log() {
+    echo "INSTALLER: ${*}" | tee -a /tmp/bootstrap.log
+}
 
-# Remove the jta package if it is installed on the system
+log "Remove the jta package if it is installed on the system"
 yum remove jta -y
 echo "exclude=jta*" >> /etc/yum.conf
 yum-config-manager --disable ol7_addons
 
-# Install the latest oracle-release-el7 package
+log "Install the latest oracle-release-el7 package"
 yum install oracle-release-el7 -y
 
-# Resolve issue with "Delta RPMs disabled because /usr/bin/applydeltarpm not installed"
+log "Resolve issue with 'Delta RPMs disabled because /usr/bin/applydeltarpm not installed'"
 yum install deltarpm -y
 echo "deltarpm=0" >> /etc/yum.conf
 
-# Install and enable the Oracle Linux Manager yum repository
+log "Install and enable the Oracle Linux Manager yum repository"
 yum install oracle-linux-manager-server-release-el7 -y
 yum-config-manager --enable ol7_optional_latest -y
 
-# Install the following packages for enabling Oracle Linux Manager server to use PostgreSQL
+log "Install the following packages for enabling Oracle Linux Manager server to use PostgreSQL"
 yum install spacewalk-setup-postgresql spacewalk-postgresql spacecmd spacewalk-utils -y
 
-# Configure Oracle Linux Manager using the answer file
+log "Configure Oracle Linux Manager using the answer file"
 spacewalk-setup --answer-file=/tmp/answer-file.txt
