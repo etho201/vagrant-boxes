5723f0861334323577a572e937561e99901c9a3c
Erik Thomsen <ethomsen86@gmail.com>
2022-02-27 00:25:18 -0500

    Added vnc install script

---
+++ ol8-gnome/scripts/install-vnc.sh
@@ -0,0 +1,24 @@
+dnf install -y tigervnc-server tigervnc-server-module
+
+# Set VNC password
+vncpasswd="vagrant"
+mkdir /home/vagrant/.vnc
+echo $vncpasswd | vncpasswd -f > /home/vagrant/.vnc/passwd
+chown -R vagrant:vagrant /home/vagrant/.vnc
+chmod 0600 /home/vagrant/.vnc/passwd
+
+restorecon -RFv /home/vagrant/.vnc
+
+echo ":1=vagrant" >> /etc/tigervnc/vncserver.users
+
+mkdir -p /etc/systemd/system/vncserver@.service.d/
+cat <<EOF > /etc/systemd/system/vncserver@.service.d/10-restart.conf
+[Service]
+Restart=always
+EOF
+
+systemctl daemon-reload
+systemctl enable --now vncserver@:1.service
+
+firewall-cmd --zone=public --add-service=vnc-server --permanent
+firewall-cmd --reload
