f06008e5b439742ef8d483a1baec50899182d4b6
Erik Thomsen <ethomsen86@gmail.com>
2022-05-24 16:32:29 -0400

    Fixed issues with provisioning script

--- wireguard-server/scripts/provision.sh
+++ wireguard-server/scripts/provision.sh
@@ -11,14 +11,9 @@ sysctl -p
 # Configure wg0.conf for the server
 echo "[Interface]
 Address = 192.168.33.1/24
-SaveConfig = true
-PostUp = ufw route allow in on wg0 out on eth0
-PostUp = iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE
-PostUp = ip6tables -t nat -I POSTROUTING -o eth0 -j MASQUERADE
-PreDown = ufw route delete allow in on wg0 out on eth0
-PreDown = iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
-PreDown = ip6tables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
-ListenPort = 51820
+PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE; ip6tables -A FORWARD -i wg0 -j ACCEPT; ip6tables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE; ip6tables -D FORWARD -i wg0 -j ACCEPT; ip6tables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
+ListenPort = ${LISTEN_PORT}
 PrivateKey = $(cat /etc/wireguard/privatekey)" > /etc/wireguard/wg0.conf
 
 # Create clients
@@ -28,24 +23,18 @@ mkdir -p /etc/wireguard/clients
 wg genkey | tee /etc/wireguard/clients/user1.key | wg pubkey > /etc/wireguard/clients/user1.key.pub
 echo "[Interface]
 PrivateKey = $(cat /etc/wireguard/clients/user1.key)
-Address = 192.168.33.101/32
+Address = 192.168.33.101/24
 DNS = 1.1.1.1, 1.0.0.1
 
 [Peer]
 PublicKey = $(cat /etc/wireguard/publickey)
 AllowedIPs = 0.0.0.0/0, ::/0
-Endpoint = $(curl ifconfig.me):51820" > /etc/wireguard/clients/user1.conf
+Endpoint = $([[ ${DOMAIN} = false ]] && curl ifconfig.me || echo ${FQDN}):${LISTEN_PORT}" > /etc/wireguard/clients/user1.conf
 
 ## Add clients to server config (wg0.conf)
 echo "[Peer]
 PublicKey = $(cat /etc/wireguard/clients/user1.key.pub)
-AllowedIPs = 0.0.0.0/0, ::/0" >> /etc/wireguard/wg0.conf
-
-# ## Do we need to specify the endpoint? (not on mobile phone)
-# echo "[Peer]
-# PublicKey = $(cat /etc/wireguard/clients/user1.key.pub)
-# AllowedIPs = 0.0.0.0/0, ::/0
-# Endpoint = $(curl ifconfig.me)" >> /etc/wireguard/wg0.conf
+AllowedIPs = 192.168.33.101/24" >> /etc/wireguard/wg0.conf
 
 # Start wg0
 wg-quick up wg0
