fcedfd9d5e77a77eaffa1510e633c34063bed93f
Erik Thomsen <ethomsen86@gmail.com>
2022-05-30 00:02:30 -0400

    Added ability to update DuckDNS IP

--- wireguard-server/scripts/provision.sh
+++ wireguard-server/scripts/provision.sh
@@ -23,18 +23,18 @@ mkdir -p /etc/wireguard/clients
 wg genkey | tee /etc/wireguard/clients/user1.key | wg pubkey > /etc/wireguard/clients/user1.key.pub
 echo "[Interface]
 PrivateKey = $(cat /etc/wireguard/clients/user1.key)
-Address = 192.168.33.101/24
+Address = 192.168.33.101/32
 DNS = 1.1.1.1, 1.0.0.1
 
 [Peer]
 PublicKey = $(cat /etc/wireguard/publickey)
 AllowedIPs = 0.0.0.0/0, ::/0
-Endpoint = $([[ ${DOMAIN} = false ]] && curl ifconfig.me || echo ${FQDN}):${LISTEN_PORT}" > /etc/wireguard/clients/user1.conf
+Endpoint = $([[ ${DUCKDNS} = false ]] && curl ifconfig.me || echo ${FQDN}):${LISTEN_PORT}" > /etc/wireguard/clients/user1.conf
 
 ## Add clients to server config (wg0.conf)
 echo "[Peer]
 PublicKey = $(cat /etc/wireguard/clients/user1.key.pub)
-AllowedIPs = 192.168.33.101/24" >> /etc/wireguard/wg0.conf
+AllowedIPs = 192.168.33.101/32" >> /etc/wireguard/wg0.conf
 
 # Configure firewall
 systemctl enable --now firewalld
@@ -45,3 +45,9 @@ systemctl enable --now wg-quick@wg0
 
 # Generate QR code for easy connection
 qrencode -t ansiutf8 < /etc/wireguard/clients/user1.conf
+
+# Add entry in crontab to update DuckDNS
+if [[ ${DUCKDNS} = true ]]; then
+    curl -sk "https://www.duckdns.org/update?domains=${SUBDOMAIN}&token=${TOKEN}&ip=" > /dev/null
+    (crontab -l 2>/dev/null; echo "*/5 * * * * curl -sk \"https://www.duckdns.org/update?domains=${SUBDOMAIN}&token=${TOKEN}&ip=\" > /dev/null") | crontab -
+fi
